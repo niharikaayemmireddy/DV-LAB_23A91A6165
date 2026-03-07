ca31107e2745c6847ff944a2d492c069008774e1
Niharika <niharikaayemmireddyy87@gmail.com>
2026-03-07 10:07:31 +0530

    Week 4B

---
+++ scripts/Week 4B.R
@@ -0,0 +1,41 @@
+# DV Lab – Week 04b
+# Name:Niharika Yemmireddy
+# Roll No:23A91A6165
+# Load the Air Quality Dataset
+data(airquality)
+
+# Understand the dataset
+str(airquality)
+View(airquality)
+class(airquality)
+?airquality
+
+# Basic Box Plot for Ozone
+boxplot(airquality$Ozone,
+        main = "Box Plot of Ozone Levels",
+        ylab = "Ozone (ppb)",
+        col = "skyblue")
+
+# Box Plot for Solar Radiation
+boxplot(airquality$Solar.R,
+        main = "Box Plot of Solar Radiation",
+        ylab = "Solar Radiation",
+        col = "lightgreen")
+
+# Box Plot for Wind Speed
+boxplot(airquality$Wind,
+        main = "Box Plot of Wind Speed",
+        ylab = "Wind (mph)",
+        col = "orange")
+
+# Box Plot for Temperature
+boxplot(airquality$Temp,
+        main = "Box Plot of Temperature",
+        ylab = "Temperature (°F)",
+        col = "pink")
+
+# Multi-Variable Box Plot
+boxplot(airquality[, c("Ozone", "Solar.R", "Wind", "Temp")],
+        main = "Air Quality Parameters Box Plot",
+        col = c("skyblue", "lightgreen", "orange", "pink"),
+        ylab = "Values")
